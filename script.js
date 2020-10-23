8898fa12b64c99146591b1dfae3208dc28e9921e
Lorna Bogan <lorna.bogan@gmail.com>
2020-10-23 14:32:17 -0500

    git commit -m "refactor:  change const to let"

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".form__close-button");
-const form = document.querySelector(".form");
-const modal= document.querySelector('.popup');
-const nameInput = document.querySelector('.form__input_type_name');
-const jobInput = document.querySelector('.form__input_type_job');
-const profileName = document.querySelector('.profile__info-title');
-const profileJob = document.querySelector('.profile__info-subtitle');
+let editButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".form__close-button");
+let form = document.querySelector(".form");
+let modal= document.querySelector('.popup');
+let nameInput = document.querySelector('.form__input_type_name');
+let jobInput = document.querySelector('.form__input_type_job');
+let profileName = document.querySelector('.profile__info-title');
+let profileJob = document.querySelector('.profile__info-subtitle');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
