e30d7b43cbbedb8335d094f2ea0c8e373a6cd153
Lorna Bogan <lorna.bogan@gmail.com>
2020-11-05 16:44:29 -0600

    javasript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Modals
-let form = document.querySelector(".form");
-let modal= document.querySelector('.popup');
+let editForm = document.querySelector(".edit-form");
+let addForm = document.querySelector(".add-form");
+let editModal= document.querySelector('.popup_type_edit');
+let addModal = document.querySelector('.popup_type_add-card');
 
 //Buttons and other DOM elements
 let editButton = document.querySelector(".profile__edit-button");
@@ -9,37 +11,75 @@ let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
 let profileName = document.querySelector('.profile__info-title');
 let profileJob = document.querySelector('.profile__info-subtitle');
+//let cardTitle = document.querySelector('.form__input_type_title');
+//let cardImage = document.querySelector('.form__input_type_link');
+
+const cardTemplate = document.querySelector('.card__template').content.querySelector('.elements__element');
+const list = document.querySelector('.elements');
+
+
 let addButton = document.querySelector('.profile__add-button');
+let addCloseButton = document.querySelector('.form__close-button');
 let titleInput = document.querySelector('.form__input_type_title');
 let linkInput = document.querySelector('.form__input_type_link');
 
+const newCard =[{
+  name: card.name,
+  link: card.link
+}];
+
 // Edit Form Features
-form.addEventListener('submit', (event) => {
+editForm.addEventListener('submit', (event) => {
   event.preventDefault();
  
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   
-  toggleModal()
-})
+  toggleModal(editModal)
+});
 
 //Add Button Form Features
-form.addEventListener('submit', (evt) => {
+addForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
-  cardTitle.textContent = titleInput.value;
-  cardImage.textContent = linkInput.value;
-})
+  newCard.forEach(card => {
+  
+    const cardElement = cardTemplate.cloneNode(true);
+  
+    const cardTitle = cardElement.querySelector('.elements__caption');
+    const cardImage = cardElement.querySelector('.elements__element-pic');
+    const cardLike = cardElement.querySelector('.elements__favorite');
+    const cardTrash = cardElement.querySelector('.elements__trash');
+  
+    cardTitle.textContent = card.name;
+    cardImage.setAttribute('src', card.link);
+  
+    list.prepend(cardElement);
+  });
 
+  toggleModal(addModal)
+});
+ 
+addCloseButton.addEventListener('click', () => {
+  addForm.classList.remove('.popup_open');
+});
+
+closeButton.addEventListener('click', () => {
+});
 
 //Close Button Features
-function toggleModal(){
+function toggleModal(modal){
   modal.classList.toggle('popup_open');
-}
+};
 
-editButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
-addButton.addEventListener('click', toggleModal);
+editButton.addEventListener('click', () => {
+  toggleModal(editModal);
+});
+
+
+addButton.addEventListener('click', () => {
+  toggleModal(addModal);
+});
 
 //Cards to be loaded to browser
 const initialCards = [
@@ -69,10 +109,13 @@ const initialCards = [
     }
   ]; 
 
+//loading card templates to the browser
+
 
 initialCards.forEach(card => {
-  const cardTemplate = document.querySelector('.card__template').content.querySelector('.elements__element');
+  
   const cardElement = cardTemplate.cloneNode(true);
+
   const cardTitle = cardElement.querySelector('.elements__caption');
   const cardImage = cardElement.querySelector('.elements__element-pic');
   const cardLike = cardElement.querySelector('.elements__favorite');
@@ -81,7 +124,7 @@ initialCards.forEach(card => {
   cardTitle.textContent = card.name;
   cardImage.setAttribute('src', card.link);
 
-  const list = document.querySelector('.elements__element');
   list.prepend(cardElement);
-})
+});
+ 
 
