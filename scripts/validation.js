20bf09596f6fee11f6e32721667ef4aca723a878
Lorna Bogan <lorna.bogan@gmail.com>
2020-11-17 15:45:58 -0600

    Validation initial commit

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,48 +1,74 @@
-function showErrorMessage(input, form, rest) {
-    const error = document.querySelector
+function showErrorMessage(input, {errorClass, inputErrorClass, ...rest}) {
+    const error = document.querySelector('#' + input.id + '-error');
+    error.textContent = input.validationMessage;
 
+    error.classList.add(errorClass);
+    input.classList.add(inputErrorClass);
 }
 
-function checkInputValidity() {
+function hideErrorMessage(input, {errorClass, inputErrorClass, ...rest}) {
+    const error = document.querySelector('#' + input.id + '-error');
+    error.textContent = '';
+
+    error.classList.remove(errorClass);
+    input.classList.remove(inputErrorClass);
+}
+
+function checkInputValidity(input, form, rest) {
     if(input.validity.valid) {
         //hide error message
+        hideErrorMessage(input, form, rest)
     } else {
         //show error message
+        showErrorMessage(input, form, rest)
     }
+}
 
+function toggleButtonState(inputs, button, {inactiveButtonClass, ...rest}) {
+    const isValid = inputs.every((input) => input.validity.valid)
 
+    if (isValid) {
+        button.classList.remove(inactiveButtonClass);
+
+    } else {
+        button.classList.add(inactiveButtonClass);
+    }
 }
 
 
-function enableValidation(formSelector, inputSelector, submitButtonSelector, ...rest){
-    const forms = [...document.querySelectorAll(settings.formSelector)];
-    forms.forEach(form) => {
+function enableValidation({formSelector, inputSelector, submitButtonSelector, ...rest}) {
+    const forms = [...document.querySelectorAll(formSelector)];
+
+    forms.forEach((form) => {
         form.addEventListener('submit', ((e) => {
             e.preventDefault()
         }))
-        const inputs = [...document.querySelectorAll(settings.inputSelector)];
-        const button = form.querySelector(settings.submitButtonSelector);
 
-        inputs.forEach(input) => {
+        const inputs = [...form.querySelectorAll(inputSelector)];
+        const button = form.querySelector(submitButtonSelector);
+
+        inputs.forEach((input) => {
             input.addEventListener('input', () => {
                 //check Input Validity
-                checkInputValidity(input, form, rest)
+                checkInputValidity(input, form, rest);
                 //toggle button state
-
+                toggleButtonState(inputs, button, rest);
             })
-        }
-    }
-
+       })
+    })
 }
+
 
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+
+//call enableValidation
 enableValidation({
     formSelector: ".popup__form",
     inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
+    submitButtonSelector: ".popup__save",
+    inactiveButtonClass: "popup__save_disabled",
     inputErrorClass: "popup__input_type_error",
     errorClass: "popup__error_visible"
-  }); 
+});
