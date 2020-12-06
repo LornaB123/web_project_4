7946b399836662405072a235fa125c9af6f9831b
Lorna Bogan <lorna.bogan@gmail.com>
2020-12-06 15:26:00 -0600

    changing setEventListeners

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,4 @@
-import {openModal, imageModal} from "./utils.js";
+import {openModal} from "./utils.js";
 
 const imagePopup = document.querySelector('.popup_type_image');
 const popupPic = imagePopup.querySelector('.popup__image');
@@ -22,30 +22,41 @@ export default class Card {
       popupCaption.textContent = this._name; 
     } 
 
+  //   function imageModal(link, name) { 
+  //   const imagePopup = document.querySelector('.popup_type_image');
+  //   const popupPic = imagePopup.querySelector('.popup__image');
+  //   const popupCaption = imagePopup.querySelector('.popup__caption');
+  //   popupPic.setAttribute('src', link); 
+  //   popupPic.setAttribute('alt', name); 
+  //   popupCaption.textContent = name; 
+  // } 
+
+    _cardTrashSelector (e) {
+        e.target.closest(".elements__element").remove();
+    }
+
+    _cardLikeSelector (e) {
+        e.target.classList.toggle('elements__favorite_selected');
+    }
+
+    _cardImageSelector () {
+        _imageModal(this._link, this._name); 
+        openModal(imagePopup); 
+    }
+
     _setEventListeners() {
-        
-        const cardImage = this._cardElement.querySelector('.elements__element-pic'); 
-        const cardLike = this._cardElement.querySelector('.elements__favorite'); 
-        const cardTrash = this._cardElement.querySelector('.elements__trash');
-
-        cardTrash.addEventListener('click', (e) => { 
-            e.target.closest('.elements__element').remove(); 
-            }); 
-
-        cardLike.addEventListener('click', (e) => { 
-           e.target.classList.toggle('elements__favorite_selected'); 
-        }); 
-
-        cardImage.addEventListener('click', (e) =>{      
-            imageModal(this._link, this._name); 
-            openModal(imagePopup); 
-           }); 
+        this._cardTrash.addEventListener("click", this._cardTrashSelector());
+        this._cardLike.addEventListener("click", this._cardLikeSelector()); 
+        this._cardImage.addEventListener('click', this._cardImageSelector()); 
     }
 
     createCard() { 
         this._cardElement = this._getCardTemplate();
         const cardImage = this._cardElement.querySelector('.elements__element-pic');
         const cardTitle = this._cardElement.querySelector('.elements__caption'); 
+        this._cardTrash = this._cardElement.querySelector('.elements__trash');
+        this._cardLike = this._cardElement.querySelector('.elements__favorite'); 
+        this._cardImage = this._cardElement.querySelector('.elements__element-pic'); 
             
         cardTitle.textContent = this._name; 
         cardImage.setAttribute('src', this._link); 
