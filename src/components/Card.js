d67aed12303b2a03249545127ea93435add7fa88
Lorna Bogan <lorna.bogan@gmail.com>
2021-01-01 11:48:41 -0600

    new card not working

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
-//import {openModal} from "../utils/utils.js";
-const imageModalPopup = document.querySelector('.popup_type_image');
-const popupPic = imageModalPopup.querySelector('.popup__image');
-const popupCaption = imageModalPopup.querySelector('.popup__caption');
+// //import {openModal} from "../utils/utils.js";
+// const imageModalPopup = document.querySelector('.popup_type_image');
+// const popupPic = imageModalPopup.querySelector('.popup__image');
+// const popupCaption = imageModalPopup.querySelector('.popup__caption');
 
 
 export default class Card {
@@ -17,22 +17,22 @@ export default class Card {
     }
 
 
-    _imageModal() { 
-       popupPic.setAttribute('src', this._link); 
-       popupPic.setAttribute('alt', this._name); 
-       popupCaption.textContent = this._name; 
-     } 
+    // _imageModal() { 
+    //    popupPic.setAttribute('src', this._link); 
+    //    popupPic.setAttribute('alt', this._name); 
+    //    popupCaption.textContent = this._name; 
+    //  } 
 
-     _cardImageSelector(){
-         this._imageModal();
-         openModal(imageModalPopup);
-     }
+    //  _cardImageSelector(){
+    //      this._imageModal();
+    //      openModal(imageModalPopup);
+    //  }
 
-    _cardTrashSelector(e){
+    _handleTrashClick(e){
         e.target.closest('.elements__element').remove(); 
     }
 
-    _cardLikeSelector(e){
+    _handleCardLike(e){
         e.target.classList.toggle('elements__favorite_selected'); 
     }
 
@@ -44,15 +44,15 @@ export default class Card {
 
     createCard() { 
         this._cardElement = this._getCardTemplate();
-        const cardImage = this._cardElement.querySelector('.elements__element-pic');
-        const cardTitle = this._cardElement.querySelector('.elements__caption'); 
+        //const cardImage = this._cardElement.querySelector('.elements__element-pic');
+        this._cardTitle = this._cardElement.querySelector('.elements__caption'); 
         this._cardImage = this._cardElement.querySelector('.elements__element-pic'); 
         this._cardLike = this._cardElement.querySelector('.elements__favorite'); 
         this._cardTrash = this._cardElement.querySelector('.elements__trash');
             
-        cardTitle.textContent = this._name; 
-        cardImage.setAttribute('src', this._link); 
-        cardImage.setAttribute('alt', this._name); 
+        this._cardTitle.textContent = this._name; 
+        this._cardImage.setAttribute('src', this._link); 
+        this._cardImage.setAttribute('alt', this._name); 
 
         this._setEventListeners();
 
